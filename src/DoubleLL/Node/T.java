a683038ae38bdbe09af1df8c2729ad02d14c5f4d
73azn <amerbinazim@gmail.com>
2023-10-02 12:36:03 +0300

    v3.1.2
    crated all the method that needed and eliminated all of the possible errors
    
    Reviewed-by: 73azn <amerbinazim@gmail.com>

--- src/DoubleLL/Node/T.java
+++ src/DoubleLL/Node/T.java
@@ -3,19 +3,22 @@ package DoubleLL.Node;
 public class T {
     Node head ;
     Node tail;
-    //insert
+
+
+
+    //insert start
     void insertFirst(int data){
+
         Node newNode = new Node(data);
         if (isEmpty()){
             head =tail = newNode;
             return;
         }
+
         newNode.next = head;
         newNode.next.prv = newNode;
         newNode.prv = null;
         head = newNode;
-
-
 
     }
 
@@ -41,9 +44,11 @@ public class T {
             System.out.println("no node with this value "+ value);
             return;
         }
+
         for (;cur!=null&&cur.data !=value;cur=cur.next);
 
         if (cur == null){
+
             System.out.println("no node with this value "+value);
             return;
         }
@@ -64,7 +69,76 @@ public class T {
     }
     //insert end
 
+    //Deleting start
+    void deleteFirst (){
+        if (isEmpty()){
+            System.out.println("the set are empty");
+            return;
+        }
+        if (isHeadEqTail()){
+            head =tail=null;
+            return;
+        }
+        head = head.next;
+        head.prv = null;
 
+    }
+    void deleteLast(){
+        if (isEmpty()){
+            System.out.println("the set are empty");
+            return;
+        }
+
+        if (isHeadEqTail()){
+            head =tail=null;
+            return;
+        }
+
+        tail.prv.next = null;
+        tail = tail.prv;
+
+
+    }
+    void deleteAfterNode(int value){
+        Node cur = head;
+        if (isEmpty()){
+            System.out.println("the set are empty");
+            return;
+        }
+        for (; cur!= null && cur.data != value;cur = cur.next);
+
+        if (cur == null){
+            System.out.println("no node with this value "+ value);
+            return;
+        }
+
+
+
+        if (cur == head){
+            cur.next = cur.next.next;
+            cur.next.prv = head;
+            return;
+        }
+        if (cur == tail){
+            System.out.println("no node after "+ value);
+            return;
+        }
+        if (cur.next == tail){
+            deleteLast();
+            return;
+        }
+        if (cur.data == value){
+            cur.next = cur.next.next;
+            cur.next.prv = cur;
+
+        }
+        else {
+            System.out.println("error");
+        }
+    }
+    //Deleting end
+
+    //display start
     void displayList(){
         Node cur = head;
 
@@ -91,6 +165,7 @@ public class T {
 
 
     void displayTail(){
+
         if (isEmpty()){
 
             return;
@@ -100,11 +175,88 @@ public class T {
 
     }
 
+    void displayCount(){
+        System.out.println("the amount of nodes are "+Count());
+    }
+
+    void displayPreSuss(int value){
+        Node cur = head;
+
+        for (; cur!=null&&cur.data!= value;cur=cur.next);
+
+        if (cur == null){
+            System.out.println("no Node with this Value "+value);
+            return;
+        }
+
+
+        if (cur == head){
+            System.out.println("there is no pre");
+            System.out.println("the suss are "+head.next.data);
+            return;
+        }
+        if (cur == tail){
+            System.out.println("the pre are "+ tail.prv.data);
+            System.out.println("there is no suss");
+            return;
+        }
+        if (cur.data == value){
+            System.out.println("the pre are "+cur.prv.data);
+            System.out.println("the suss are "+ cur.next.data);
+            return;
+        }
 
 
 
+
+        System.out.println("there is an error");
+
+
+
+    }
+    //display end
+
+    //Traversing start
+    int Count (){
+        Node cur = head;
+        if (isEmpty()){
+            return 0;
+
+        }
+        int count =0;
+        for (;cur != null; cur= cur.next){
+            count++;
+        }
+        return count;
+    }
+    //Traversing end
+
+
+
+    //general methods
     boolean isEmpty(){
         return head == null;
+    }
+
+    boolean isHeadEqTail(){
+
+        return head == tail;
+    }
+
+    boolean validNum(int value) {
+        Node cur = head;
+
+
+        while (cur != null) {
+
+            if (cur.data == value) {
+                return true;
+            }
+            cur = cur.next;
+        }
+
+
+        return false;
     }
 
 }
--- src/DoubleLL/Node/test_T.java
+++ src/DoubleLL/Node/test_T.java
@@ -7,12 +7,85 @@ public class test_T {
         T myList = new T();
 
 
-        myList.insertLast(2);
+        System.out.println("test 1\n======================================");
+        // Test 1: Insert at the beginning
+        myList.insertFirst(10);
+
+        myList.displayList(); // Expected output:  10
+
+        // Test 2: Insert at the end
+        myList.insertLast(20);
+        myList.displayList(); // Expected output:  10 20
+
+        // Test 3: Insert after a specific node
+        myList.insertAfterNode(25, 10);
+        myList.displayList(); // Expected output: 10 25 20
+
+        // Test 4: Delete first node
+        myList.deleteFirst();
+        myList.displayList(); // Expected output:  25 20
+
+        // Test 5: Delete last node
+        myList.deleteLast();
+        myList.displayList(); // Expected output: 25
+
+        // Test 6: Delete after a specific node
+        myList.deleteAfterNode(10); //Expected output: there is no number equals 10
+        myList.displayList(); // Expected output: 25
+        myList.displayFirst();//Expected output: The Head 25
+        myList.displayTail();//Expected output: The Tail 25
+
+        // Test 7: Display the first node
+
+
+        // Test 9: Display the count of nodes
+        myList.displayCount(); // Expected output: the number of the nodes are: 1
+        System.out.println("======================================");
+        System.out.println("test 2\n======================================");
+        // Test 10: Display previous and successor nodes
+        myList.displayList();
+        myList.insertLast(20);
+        myList.insertLast(30);
+        myList.displayPreSuss(10);
+        // Expected output:
+        // you there is no prv
+        // the Suss are 20
+        myList.displayPreSuss(20); //
+        // Expected output:
+        // the prv are 10
+        // the Suss are 30
+        myList.displayPreSuss(30);
+        // Expected output:
+        // the prv are 20
+        // no suss in here
+
+
+        myList.deleteLast();
+        myList.deleteLast();
+        myList.deleteLast();
+        myList.deleteLast();// Expected output: the list are empty
+        System.out.println("======================================");
+
+        System.out.println("test 3\n======================================");
+
         myList.insertFirst(1);
-        myList.insertAfterNode(3,5);
+        myList.insertAfterNode(2,1);
+        myList.insertLast(3);
         myList.displayList();
         myList.displayFirst();
         myList.displayTail();
+        System.out.println("======================================");
+        // Test 11: Count the number of nodes
+        int count = myList.Count();
+        System.out.println("Count: " + count); // Expected output: Count: 3
+
+        // Test 12: Check if a specific value exists
+        boolean exists = myList.validNum(20);
+        System.out.println("Value 20 exists: " + exists); // Expected output: Value 20 exists: true
+
+        // Test 13: Check if a specific value doesn't exist
+        exists = myList.validNum(40);
+        System.out.println("Value 40 exists: " + exists); // Expected output: Value 40 exists: false
     }
 
 
