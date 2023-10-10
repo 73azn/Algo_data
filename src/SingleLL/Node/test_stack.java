da7f77a7e24322196976f1d14a5238906bb25b7c
73azn <amerbinazim@gmail.com>
2023-10-10 20:50:56 +0300

    v4.0.1
    created stack and test
    
    Reviewed-by: 73azn <amerbinazim@gmail.com>

---
+++ src/SingleLL/Node/test_stack.java
@@ -0,0 +1,39 @@
+package SingleLL.Node;
+
+
+
+public class test_stack {
+    public static void main(String[] args){
+
+        stack myStack = new stack();
+
+        myStack.push(1);
+
+        myStack.push(2);
+
+        myStack.push(3);
+
+        myStack.pop();
+        myStack.reverseStack();
+
+        myStack.peek();
+
+        myStack.reverseStack();
+
+        myStack.push(1);
+        myStack.reverseStack();
+        myStack.push(2);
+        myStack.reverseStack();
+
+        myStack.push(3);
+
+        myStack.pop();
+        myStack.reverseStack();
+
+
+        myStack.peek();
+
+
+
+    }
+}
